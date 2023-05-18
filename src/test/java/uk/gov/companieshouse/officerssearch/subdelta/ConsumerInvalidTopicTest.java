6f57fe5d6b8e383c2a171863667e9d356c66bd66
johncook-ch <jcook@companieshouse.gov.uk>
2023-05-18 10:56:53 +0100

    DSND-1740: Tests fixed for positive Concourse check

--- src/test/java/uk/gov/companieshouse/officerssearch/subdelta/ConsumerInvalidTopicTest.java
+++ src/test/java/uk/gov/companieshouse/officerssearch/subdelta/ConsumerInvalidTopicTest.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.officerssearch.subdelta;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 import static uk.gov.companieshouse.officerssearch.subdelta.TestUtils.ERROR_TOPIC;
 import static uk.gov.companieshouse.officerssearch.subdelta.TestUtils.INVALID_TOPIC;
 import static uk.gov.companieshouse.officerssearch.subdelta.TestUtils.MAIN_TOPIC;
@@ -47,7 +46,6 @@ class ConsumerInvalidTopicTest {
 
     @Test
     void testPublishToInvalidMessageTopicIfInvalidDataDeserialised() throws InterruptedException, ExecutionException {
-        fail();
         //given
         embeddedKafkaBroker.consumeFromAllEmbeddedTopics(testConsumer);
 
