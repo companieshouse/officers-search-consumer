496fb3f208786c4d751abcd9512952241558a1d6
cgregoryCH <cgregory@companieshouse.gov.uk>
2025-08-07 14:25:19 +0100

    BI-14526: Remove healthcheck prefix

--- src/test/java/uk/gov/companieshouse/officerssearch/subdelta/ApplicationIT.java
+++ src/test/java/uk/gov/companieshouse/officerssearch/subdelta/ApplicationIT.java
@@ -29,7 +29,7 @@ class ApplicationIT {
 
     @Test
     void shouldReturn200FromGetHealthEndpoint() throws Exception {
-        this.mockMvc.perform(get("/officers-search-consumer/healthcheck")
+        this.mockMvc.perform(get("/healthcheck")
                         .header(REQUEST_ID.value(), "request_id"))
                 .andDo(print())
                 .andExpect(status().isOk())
