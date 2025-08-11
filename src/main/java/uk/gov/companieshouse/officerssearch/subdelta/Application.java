5a3b791c793ecc75b7450378c2ae49ccfcef0692
sthompsonCH <sthompson@companieshouse.gov.uk>
2025-08-11 12:38:57 +0100

    TEST CHANGE 2

--- src/main/java/uk/gov/companieshouse/officerssearch/subdelta/Application.java
+++ src/main/java/uk/gov/companieshouse/officerssearch/subdelta/Application.java
@@ -10,6 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TEST CHANGE
+        // TEST CHANGE 2
         SpringApplication.run(Application.class, args);
     }
 }
