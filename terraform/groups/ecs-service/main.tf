3c69dc43800f8263ebc48f9cdbd0c26e6841db6d
mike-kelly-ch <mkelly1@companieshouse.gov.uk>
2026-09-22 14:20:03 +0100

    Update terraform main for old kafka instance.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.427"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.326"
 
   # Environmental configuration
   environment             = var.environment
@@ -87,7 +87,7 @@ module "ecs-service" {
 # ------------------------------------------------------------------------------
 
 module "ecs-service-old-kafka" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.427"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.326"
   count  = var.create_old_kafka_service ? 1 : 0
   
   # Environmental configuration
@@ -152,7 +152,7 @@ module "ecs-service-old-kafka" {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.427"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.326"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
